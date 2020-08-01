67f85e8d77a79a8924449cddf8ce4414161df4b6
mbborromeo <mbborromeo@gmail.com>
2020-07-31 19:26:02 -0700

    attempt for useMemo but incorrect

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -114,112 +114,119 @@ function List() {
     [ dataService ]
     );
       
-    // possibly use useMemo here, and/or define a function for sort  
+    // possibly use useMemo here, and/or define a function for sort    
     if( list ){
         if( list.length > 0  ){
             if( sortConfig.key !== null ){
                 console.log('sorting...')
     
-                list.sort( (a, b) => {
-                    if( a[sortConfig.key] < b[sortConfig.key] ){
-                        if( sortConfig.key === 'completed' ){ // completed has reversed order
-                            return sortConfig.direction === 'ascending' ? 1 : -1;
-                        } else {
-                            return sortConfig.direction === 'ascending' ? -1 : 1;
-                        }
-                    }
-                    if( a[sortConfig.key] > b[sortConfig.key] ){
-                        if( sortConfig.key === 'completed' ){ // completed has reversed order
-                            return sortConfig.direction === 'ascending' ? -1 : 1;
-                        } else {
-                            return sortConfig.direction === 'ascending' ? 1 : -1;
-                        }                    
-                    }
-    
-                    // then sort by ID as well
-                    if( a.id < b.id ){
-                        return -1;
-                    }                 
-                    if( a.id > b.id ){
-                        return 1;
-                    }
-                    
-                    return 0;
-                });
+                const sortedList = useMemo( () => 
+                    {
+                        // sort list
+                        list.sort( (a, b) => {
+                            if( a[sortConfig.key] < b[sortConfig.key] ){
+                                if( sortConfig.key === 'completed' ){ // completed has reversed order
+                                    return sortConfig.direction === 'ascending' ? 1 : -1;
+                                } else {
+                                    return sortConfig.direction === 'ascending' ? -1 : 1;
+                                }
+                            }
+                            if( a[sortConfig.key] > b[sortConfig.key] ){
+                                if( sortConfig.key === 'completed' ){ // completed has reversed order
+                                    return sortConfig.direction === 'ascending' ? -1 : 1;
+                                } else {
+                                    return sortConfig.direction === 'ascending' ? 1 : -1;
+                                }                    
+                            }
+            
+                            // then sort by ID as well
+                            if( a.id < b.id ){
+                                return -1;
+                            }                 
+                            if( a.id > b.id ){
+                                return 1;
+                            }
+                            
+                            return 0;
+                        });
+
+                        // render DOM
+                        return (
+                            <div>
+                                <h1>TO DO</h1>
+                
+                                <AddForm addFunction={ addToDo } />
+                
+                                <table>                    
+                                    <thead>
+                                        <tr>
+                                            <td>
+                                                <button 
+                                                  type="button" 
+                                                  className={ sortConfig.key==='id' ? sortConfig.direction : '' }
+                                                  onClick={ () => requestSort('id') }
+                                                >
+                                                    ID
+                                                </button>
+                                            </td>
+                                            <td>
+                                                <button 
+                                                  type="button" 
+                                                  className={ sortConfig.key==='title' ? sortConfig.direction : '' }
+                                                  onClick={ () => requestSort('title') }
+                                                >
+                                                    Title
+                                                </button>
+                                            </td>
+                                            <td>
+                                                <button 
+                                                  type="button" 
+                                                  id="completed"
+                                                  className={ sortConfig.key==='completed' ? sortConfig.direction : '' }
+                                                  onClick={ () => requestSort('completed') }
+                                                >
+                                                    Completed
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    { 
+                                        sortedList.map( (item, i) => (
+                                            <tr key={ item.id }>
+                                                <td>
+                                                    <Link to={ '/detail/'+ item.id } 
+                                                      data-id={ item.id } 
+                                                      className={ item.completed ? 'completed' : '' }
+                                                    >
+                                                        { item.id }
+                                                    </Link>
+                                                </td>
+                                                <td>
+                                                    <Link to={ '/detail/'+ item.id } 
+                                                      data-id={ item.id } 
+                                                      className={ item.completed ? 'completed' : '' }
+                                                    >
+                                                        { item.title }
+                                                    </Link>
+                                                </td>
+                                                <td>
+                                                    <button onClick={ () => completeToDo(i) }>
+                                                        { item.completed ? 'Mark as Incomplete' : 'Mark as Completed' }
+                                                    </button>
+                                                    <button onClick={ () => deleteToDo(i) }>X</button>
+                                                </td>
+                                            </tr>
+                                        ))
+                                    }
+                                    </tbody>
+                                </table> 
+                            </div>
+                        );
+                    },
+                    [ list, sortConfig ]
+                );
             }
-          
-            return (
-                <div>
-                    <h1>TO DO</h1>
-    
-                    <AddForm addFunction={ addToDo } />
-    
-                    <table>                    
-                        <thead>
-                            <tr>
-                                <td>
-                                    <button 
-                                      type="button" 
-                                      className={ sortConfig.key==='id' ? sortConfig.direction : '' }
-                                      onClick={ () => requestSort('id') }
-                                    >
-                                        ID
-                                    </button>
-                                </td>
-                                <td>
-                                    <button 
-                                      type="button" 
-                                      className={ sortConfig.key==='title' ? sortConfig.direction : '' }
-                                      onClick={ () => requestSort('title') }
-                                    >
-                                        Title
-                                    </button>
-                                </td>
-                                <td>
-                                    <button 
-                                      type="button" 
-                                      id="completed"
-                                      className={ sortConfig.key==='completed' ? sortConfig.direction : '' }
-                                      onClick={ () => requestSort('completed') }
-                                    >
-                                        Completed
-                                    </button>
-                                </td>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        { 
-                            list.map( (item, i) => (
-                                <tr key={ item.id }>
-                                    <td>
-                                        <Link to={ '/detail/'+ item.id } 
-                                          data-id={ item.id } 
-                                          className={ item.completed ? 'completed' : '' }
-                                        >
-                                            { item.id }
-                                        </Link>
-                                    </td>
-                                    <td>
-                                        <Link to={ '/detail/'+ item.id } 
-                                          data-id={ item.id } 
-                                          className={ item.completed ? 'completed' : '' }
-                                        >
-                                            { item.title }
-                                        </Link>
-                                    </td>
-                                    <td>
-                                        <button onClick={ () => completeToDo(i) }>
-                                            { item.completed ? 'Mark as Incomplete' : 'Mark as Completed' }
-                                        </button>
-                                        <button onClick={ () => deleteToDo(i) }>X</button>
-                                    </td>
-                                </tr>
-                            ))
-                        }
-                        </tbody>
-                    </table> 
-                </div>
-            );
         } else {
             return <div>No results to display</div>;
         }
