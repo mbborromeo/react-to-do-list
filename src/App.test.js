d5e6e2db11f3bf8e2b3054b946b690bbebf50388
mbborromeo <mbborromeo@gmail.com>
2020-08-05 02:46:02 -0700

    edit test for App.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, wait, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+// Make sure loading indicator comes up first
+test('App renders loading initially', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const loading = getByText('Loading...');
+  expect( loading ).toBeInTheDocument();
+});
+
+// Wait for items to load and display
+test('Heading renders after API results are returned', () => {
+  await wait(
+    () => expect(screen.getByText('TO DO')).toBeInTheDocument()
+  );
 });
