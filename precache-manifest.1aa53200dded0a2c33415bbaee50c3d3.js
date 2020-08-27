b4ee7b170a283993a9fed180b2c2e9621491ce3a
mbborromeo <mbborromeo@gmail.com>
2020-08-27 00:22:26 -0700

    Updates

---
+++ precache-manifest.1aa53200dded0a2c33415bbaee50c3d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfdb3147fc843cdb915f53d4aecb02c6",
+    "url": "/react-to-do-list/index.html"
+  },
+  {
+    "revision": "83a20a8b8179b5835a92",
+    "url": "/react-to-do-list/static/css/main.faefd7d3.chunk.css"
+  },
+  {
+    "revision": "437ca15aca3b879699bd",
+    "url": "/react-to-do-list/static/js/2.7d801e07.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/react-to-do-list/static/js/2.7d801e07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83a20a8b8179b5835a92",
+    "url": "/react-to-do-list/static/js/main.5857a235.chunk.js"
+  },
+  {
+    "revision": "8c3d49c384a4bcf49218",
+    "url": "/react-to-do-list/static/js/runtime-main.f9d0e86f.js"
+  }
+]);
